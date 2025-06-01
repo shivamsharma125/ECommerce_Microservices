f1f215a311ebadecde50716f6960c180763f2369
Shivam Sharma <shivamsharma8228@gmail.com>
2025-06-01 09:36:41 +0530

    project setup

---
+++ src/main/java/com/shivam/ecommerce_microservices/ECommerceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.ecommerce_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shivam/ecommerce_microservices/ECommerceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.ecommerce_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
